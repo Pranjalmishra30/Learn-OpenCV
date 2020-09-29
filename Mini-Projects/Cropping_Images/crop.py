bdf2718e8c6350c692bc3430b2d46988b1ade44b
pranjalmishra30 <pranjalmishra2022@gmail.com>
2020-09-29 09:26:09 +0530

    Updated code

--- Mini-Projects/Cropping_Images/crop.py
+++ Mini-Projects/Cropping_Images/crop.py
@@ -4,14 +4,14 @@ refpt = [] #List of refrence points
 def select_roi (event,x,y,flags,param):
     global refpt #Global refrences
 
-    if event == cv2.EVENT_LBUTTONDOWN: # When the left mouse button is cliked
+    if event == cv2.EVENT_LBUTTONDOWN: # When the left mouse button is clicked
         refpt = [(x,y)]
 
     elif event == cv2.EVENT_LBUTTONUP: # When the left mouse button is released
         refpt.append((x,y)) # recording the last coordinates
         cv2.rectangle(img_main,refpt[0],refpt[1],(0,255,0),2)
         cv2.imshow("frame",img_main)
-        print("Selection Successful")
+        print("Selection Successful")    
 
 img = cv2.imread("Data/Man_United.jpeg")
 img_main = cv2.resize(img,(400,400)) #Resizing image
@@ -28,9 +28,15 @@ while True:
     cv2.imshow("frame",img_main)
     var = cv2.waitKey(0)
 
-    # Select a region , then press c to crop that portion
+    '''
+    Instructions
+    - Select a region , then press c to crop that portion.
+    - Press r to reset your selection.
+    - In the Crop mode , press s to save your cropped image or press r to reset selection
+    - Press q to exit the program.
+    '''
 
-    if var == ord('c'): # Crop selected images
+    if var == ord('C'): # Crop selected images
         
         if len(refpt)==2:
             roi = clone2[refpt[0][1]:refpt[1][1], refpt[0][0]:refpt[1][0]] # [x1:x2 , y1:y2]
@@ -41,7 +47,7 @@ while True:
             var2 = cv2.waitKey(0)
 
             if var2 == ord('s'): # Saving cropped image
-                cv2.imwrite("Data/cropped image{}.png".format(i),roi)
+                cv2.imwrite("Data/cropped image{}.png".format(i),roi) # Name of the saved image 
                 i=i+1
                 print("image saved\n")
                 cv2.destroyWindow("Crop") 
@@ -51,6 +57,10 @@ while True:
                 cv2.destroyWindow("Crop")
                 print("Reset\n")
                 img_main = clone.copy()
+    
+    elif var == ord('r'): # Reset
+        print("Reset\n")
+        img_main = clone.copy()
                            
     elif var == ord('q'): # Exit the loop
         print("Exiting ...")
